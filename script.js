d55edb182e2caa300319b81835fa965967998873
Dave-314land <dave.l.piland@gmail.com>
2020-04-28 13:01:35 -0400

    added votes by state array. Added countVotes and determineWinner functions

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 
+var winnerMessage;
 
-var createPolitician = function (fullName, numVotes) {
+var createPolitician = function (fullName) {
     var politician = {
         name : fullName,
         electionResults : null,
-        totalVotes : numVotes,
-        announcePolitician : function() {
-            console.log(this.name + " is running for office! He has " + this.totalVotes + " votes." );
-        }
+        totalVotes : 0,
+        countVotes : function () {
+            for (i=0; i < this.electionResults.length; i++) {
+                this.totalVotes = this.totalVotes + this.electionResults[i];
+            } 
+        },
     };
-
-    politician.announcePolitician();
     return politician;
 };
 
-var politician1 = createPolitician("Dave", 2);
+var politician1 = createPolitician("Jim");
+var politician2 = createPolitician("Dwight");
+
+politician1.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+politician2.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+
+politician1.countVotes();
+politician2.countVotes();
+
+var determineWinner = function() {
+    if (politician1.totalVotes > politician2.totalVotes) {
+        winnerMessage = politician1.name + " is the winner with " + politician1.totalVotes + " votes!";
+    }
+
+    else if (politician1.totalVotes < politician2.totalVotes) {
+        winnerMessage = politician2.name + " is the winner with " + politician2.totalVotes + " votes!";
+    }
+
+    else {
+        winnerMessage = "It's a draw!";
+    }
+};
+
+determineWinner();
+
+console.log(winnerMessage);
