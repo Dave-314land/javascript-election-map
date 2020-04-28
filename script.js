ada7bd850c1662a43802488438a27c1ecbf0f5d2
Dave-314land <dave.l.piland@gmail.com>
2020-04-28 09:26:57 -0400

    wrapped politician object literal within a factory function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
-var politician = {
-    name:"",
-    electionResults: null,
-    totalVotes: 0
+
+var createPolitician = function (fullName, numVotes) {
+    var politician = {
+        name : fullName,
+        electionResults : null,
+        totalVotes : numVotes,
+        announcePolitician : function() {
+            console.log(this.name + " is running for office! He has " + this.totalVotes + " votes." );
+        }
+    };
+
+    politician.announcePolitician();
+    return politician;
 };
+
+var politician1 = createPolitician("Dave", 2);
